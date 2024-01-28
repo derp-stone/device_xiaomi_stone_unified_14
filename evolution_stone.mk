553c5c03fc1a7d31deaf3f9e55c84519d78d6cfe
Drpto <dr.juniornoob@gmail.com>
2024-01-28 14:19:05 +0530

    stone: Go with EvolutionX Official
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- evolution_stone.mk
+++ evolution_stone.mk
@@ -9,26 +9,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Inherit from stone device
 $(call inherit-product, device/xiaomi/stone/device.mk)
 
+#EvolutionX prop
+EVO_BUILD_TYPE := OFFICIAL
+
 # Device props
-TARGET_SUPPORTS_BLUR := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_DISABLE_EPPE := true
 TARGET_DEBLOAT := true
 
 # Charging Animation
-TARGET_USE_PIXEL_CHARGER := true
+USE_PIXEL_CHARGER := true
 
 # Use Prebuilt Kernel
 PREBUILT_KERNEL := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_stone
+PRODUCT_NAME := evolution_stone
 PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+# Device Repos [1/2]
+git clone --depth=1 https://github.com/Stone-Trees/vendor_xiaomi.git -b lineage-21 vendor/xiaomi
+
+git clone --depth=1 https://github.com/Stone-Trees/xiaomi_stone-kernel.git device/xiaomi/stone-kernel
+
+git clone --depth=1 https://github.com/Stone-Trees/hardware_xiaomi.git hardware/xiaomi
+
+#viper4Fx Repo [2/2]
+git clone --depth=1 https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
